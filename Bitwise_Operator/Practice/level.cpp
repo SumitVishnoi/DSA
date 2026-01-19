afd1b60825cdc6715dce6f574fe8e718836e5c38
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2026-01-19 14:57:38 +0530

    add level_1 questions

--- Bitwise_Operator/Practice/level.cpp
+++ Bitwise_Operator/Practice/level.cpp
@@ -16,13 +16,67 @@ int main() {
     // cout<<"Result is: "<<result<<endl;
     // -----------------------------------------------
 
-    int n, i;
-    cout << "Enter number: ";
-    cin >> n;
-    cout << "Enter bit position: ";
-    cin >> i;
+    // Q4
 
-    int result = n & ~(1 << i);
+    // int n, i;
+    // cout << "Enter number: ";
+    // cin >> n;
+    // cout << "Enter bit position: ";
+    // cin >> i;
 
-    cout << "Number after clearing ith bit: " << result;
+    // int result = n & ~(1 << i);
+
+    // cout << "Number after clearing i-th bit: " << result;
+    // ------------------------------------------------------------
+
+    // Q5
+
+    // int n, i;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+
+    // cout<<"Enter the i-th bit: ";
+    // cin>>i;
+
+    // int result = n ^ (1<<i);
+    // cout<<"Number after toggle i-th bit: "<<result;
+    // -----------------------------------------------------
+
+    // Q6
+
+    // int n;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+
+    // cout<<"Multiply by 2: "<<(n<<1)<<endl;
+    // cout<<"Divide by 2: "<<(n>>1)<<endl;
+    // --------------------------------------------------------
+
+    // Level-1
+
+    // Q7
+
+    // int n;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+
+    // int i = n/2;
+    // if((n>>i) == 1) {
+    //     cout<<"Power of 2";
+    // }
+    // else  {
+    //     cout<<"Not power of 2";
+    // }
+
+    // ------------ or----------------
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    if(n>0 && (n&(n-1)) == 0) {
+        cout<<"Power of 2";
+    }
+    else {
+        cout<<"Not power of 2";
+    }
 }

c1d0d40659e28d9712ff8299191cd128a38f8f2b
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2026-01-21 12:39:55 +0530

    sovle leve_2 questions in bitwise

--- Bitwise_Operator/Practice/level_2.cpp
+++ Bitwise_Operator/Practice/level_2.cpp
@@ -20,14 +20,49 @@ int main() {
 
     // Q11
 
+    // int n;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+
+    //  if((n&3) == 0) {
+    //     cout<<"Divisible by 4";
+    // }
+    // else {
+    //     cout<<"Not divisible by 4";
+    // }
+    // -------------------------------------------
+
+    // Q11
+
+    // int n;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+
+    // if((n&7) == 0) {
+    //     cout<<"Divisble by 8";
+    // }
+    // else {
+    //     cout<<"Not divisible by 8";
+    // }
+    // -------------------------------------------
+
+    // Q12
     int n;
     cout<<"Enter the number: ";
     cin>>n;
 
-    if((n&(n>>2))==0) {
-        cout<<"Divisible by 4";
+    if(n == 0) {
+        cout<<"No set bit";
+        return 0;
     }
-    else {
-        cout<<"Not divisible by 4";
+
+    int pos = 0;
+    int rightmost = (n&(-n));
+
+    while(rightmost > 1) {
+        rightmost = rightmost >> 1;
+        pos++;
     }
+
+    cout<<"Position of rightmost set bit: "<<pos<<endl;
 }

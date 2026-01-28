b2526c0b53c4c73e660a33035c65a6c6afe45d1a
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2026-01-28 12:00:41 +0530

    solve level_3 questions in bitwise

--- Bitwise_Operator/Practice/level_3.cpp
+++ Bitwise_Operator/Practice/level_3.cpp
@@ -3,15 +3,62 @@ using namespace std;
 
 int main()
 {
-    int a, b;
-    cout << "Enter the value of a and b: ";
-    cin >> a >> b;
+    // Q18
 
-    while(b != 0) {
-        int carry = a&b;
-        a = a^b;
-        b = carry<<1;
-    }
+    // int n;
+    // cout<<"Enter the number: ";
+    // cin>>n;
 
-    cout<<"Sum is: "<<a<<endl;
+    // if(n==0) {
+    //     cout<<"Trailing zeros are undefined for 0";
+    // }
+
+    // n = n&(-n);
+    // int count = 0;
+    // while(n!=1) {
+    //     n = n>>1;
+    //     count++;
+    // }
+
+    // cout<<"Trailing of zeros: "<<count<<endl;
+    // -------------------------------------------------
+
+    // Q19
+    // int n;
+    // cout << "Enter the number: ";
+    // cin >> n;
+
+    // if (n == 0)
+    // {
+    //     cout << "MSB none";
+    //     return 0;
+    // }
+
+    //  n = abs(n);   // handle negative numbers
+
+    // int pos = 0;
+    // while (n > 1) {
+    //     n = n >> 1;
+    //     pos++;
+    // }
+
+    // cout << "MSB position is: " << pos;
+    // -------------------------------------------------
+
+    // Q20
+    
+    // ---------------------------------------------
+    // Q22
+
+    // int a, b;
+    // cout << "Enter the value of a and b: ";
+    // cin >> a >> b;
+
+    // while(b != 0) {
+    //     int carry = a&b;
+    //     a = a^b;
+    //     b = carry<<1;
+    // }
+
+    // cout<<"Sum is: "<<a<<endl;
 }

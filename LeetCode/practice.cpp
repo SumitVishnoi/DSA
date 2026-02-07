c9f436ec480f49ab804b3985a364189461a927c1
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2026-02-07 23:25:20 +0530

    decimal to binary conversion and vice-versa

---
+++ LeetCode/practice.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+// bool isHappy(int n) {
+//     int sum = 0;
+//     while(sum != 0) {
+//         int a = n%10;
+//         sum = sum + pow(a, 2);
+//         n = n/10;
+//     }
+
+//     if(sum == 1) {
+//         return true;
+//     }
+//     return false;
+// }
+
+// int main() {
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>> n;
+
+//     cout<<isHappy(n)<<endl;
+// }
+// ------------------------------------------------------
+
+// int main() {
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+
+//     int count = 0;
+//     for(int i=2; i<n; i++) {
+//         if(n%i == 0) {
+//             cout<<"Not prime"<<endl;
+//         }
+//         else {
+//             cout<<"Prime"<<endl;
+            
+//         }
+//     }
+// }
